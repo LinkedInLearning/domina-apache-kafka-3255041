d6b60c50b1b6eba7d891a77fe20dc8f4d51a2c55
Lincy Gonzalez Rojas <lincgr@gmail.com>
2023-03-16 19:34:17 +0100

    02_09 Obtener una lista de tópicos del servidor Apache Kafka usando PHP

---
+++ 02_09/app.php
@@ -0,0 +1,9 @@
+<?php
+require_once('vendor/autoload.php');
+
+use RdKafka\Producer;
+use RdKafka\Conf;
+
+$configuracion = new Conf();
+$configuracion->set('metadata.broker.list', 'localhost:9092');
+?>
